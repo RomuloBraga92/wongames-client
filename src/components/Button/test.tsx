84068bbd52a38cab382f78c7e0ddb9cbf8771e61
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-03 22:37:23 -0300

    feat: add button component tests

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,13 +1,61 @@
-import { render, screen } from '@testing-library/react'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
 
 import Button from './'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
+  it('should render a large size by default', () => {
+    renderWithTheme(<Button>Buy now</Button>)
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      'font-size': '1.6rem',
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a small size when passed', () => {
+    renderWithTheme(<Button size="small">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '3rem',
+    })
+  })
+
+  it('should render a medium size when passed', () => {
+    renderWithTheme(<Button size="medium">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '4rem',
+    })
+  })
+
+  it('should render a fullWidth size when passed', () => {
+    renderWithTheme(<Button fullWidth>Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      width: '100%',
+    })
+  })
+
+  it('should render an icon when passed', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>,
+    )
+
+    expect(screen.getByText(/Buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should render Button as link', () => {
+    renderWithTheme(
+      <Button as="a" href="/link">
+        Buy now
+      </Button>,
+    )
+
+    expect(screen.getByRole('link', { name: /Buy now/i })).toHaveAttribute(
+      'href',
+      '/link',
+    )
   })
 })
