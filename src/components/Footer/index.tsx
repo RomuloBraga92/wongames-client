0f9f7e8d111b665c50c9239236703566290999a6
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-06 21:55:51 -0300

    feat: add styles and content to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,84 @@
+import Link from 'next/link'
+
 import * as S from './styles'
+import Logo from 'components/Logo'
+import Heading from 'components/Heading'
 
 const Footer = () => (
   <S.Container>
-    <h1>Footer</h1>
+    <Logo color="black" />
+
+    <S.Content>
+      <S.Column>
+        <Heading bottomLine lineColor="secondary" color="black" size="small">
+          Contact
+        </Heading>
+        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading bottomLine lineColor="secondary" color="black" size="small">
+          Follow Us
+        </Heading>
+        <nav aria-labelledby="social media">
+          <a
+            href="https://www.instagram.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.twitter.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.facebook.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading bottomLine lineColor="secondary" color="black" size="small">
+          Links
+        </Heading>
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Buscar</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column aria-labelledby="footer-location">
+        <Heading bottomLine lineColor="secondary" color="black" size="small">
+          Location
+        </Heading>
+        <span>Lorem ipsum dolor sit.</span>
+        <span>Lorem Ipsum</span>
+        <span>Lorem, ipsum dolor.</span>
+      </S.Column>
+    </S.Content>
+    <S.Copyright>Won Games 2020 © All rights reserved.</S.Copyright>
   </S.Container>
 )
 
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,50 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Container = styled.main``
+import * as HeadingStyles from 'components/Heading/styles'
+
+export const Container = styled.footer`
+  ${HeadingStyles.Container} {
+    text-transform: uppercase;
+  }
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: ${theme.grid.gutter};
+    margin-top: ${theme.spacings.medium};
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: repeat(4, 1fr);
+    `}
+  `}
+`
+
+export const Column = styled.div`
+  ${({ theme }) => css`
+    a,
+    span {
+      display: block;
+      text-decoration: none;
+      color: ${theme.colors.gray};
+      margin-bottom: ${theme.spacings.xxsmall};
+      font-size: ${theme.font.sizes.small};
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  `}
+`
+
+export const Copyright = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray};
+    margin-top: ${theme.spacings.large};
+    margin-bottom: ${theme.spacings.medium};
+    font-size: ${theme.font.sizes.xsmall};
+    text-align: center;
+  `}
+`
