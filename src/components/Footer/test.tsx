5afae86ff355502d0af578260b9fe7969938a003
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-06 22:02:03 -0300

    feat: add footer tests

--- src/components/Footer/test.tsx
+++ src/components/Footer/test.tsx
@@ -1,13 +1,24 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Footer from './'
 
 describe('<Footer />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Footer />)
+  it('should render 4 columns items', () => {
+    renderWithTheme(<Footer />)
 
-    expect(screen.getByRole('heading', { name: /Footer/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Contact/i }),
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /Follow us/i }),
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /Links/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Location/i }),
+    ).toBeInTheDocument()
   })
 })
