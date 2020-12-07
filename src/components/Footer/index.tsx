d00a62eeb1505abbba9048523fe534c902476287
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-06 21:24:20 -0300

    feat: add footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Container>
+    <h1>Footer</h1>
+  </S.Container>
+)
+
+export default Footer
---
+++ src/components/Footer/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Footer from './'
+
+export default {
+  title: 'Footer',
+  component: Footer,
+} as Meta
+
+export const Default: Story = () => <Footer />
