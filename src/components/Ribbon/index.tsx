674be7eea588aac9052b74fde7db08a295ad5c42
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-07 22:39:45 -0300

    feat: add ribbon component

---
+++ src/components/Ribbon/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Ribbon = () => (
+  <S.Container>
+    <h1>Ribbon</h1>
+  </S.Container>
+)
+
+export default Ribbon
---
+++ src/components/Ribbon/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Ribbon from './'
+
+export default {
+  title: 'Ribbon',
+  component: Ribbon,
+} as Meta
+
+export const Default: Story = () => <Ribbon />
