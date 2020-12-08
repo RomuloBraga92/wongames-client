8297de6f5fef74803fbb4cc6d60a517a416ea340
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-08 00:16:00 -0300

    feat: add styles to story component

--- src/components/Ribbon/stories.tsx
+++ src/components/Ribbon/stories.tsx
@@ -11,7 +11,18 @@ export default {
   },
 } as Meta
 
-export const Default: Story<RibbonProps> = args => <Ribbon {...args} />
+export const Default: Story<RibbonProps> = args => (
+  <div
+    style={{
+      width: '40rem',
+      height: '25rem',
+      position: 'relative',
+      background: 'lightgray',
+    }}
+  >
+    <Ribbon {...args} />
+  </div>
+)
 
 Default.args = {
   children: 'Best Seller',
