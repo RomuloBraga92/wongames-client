174a5f5067255ec4c1dd7113f087396534680c06
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-13 01:09:58 -0300

    feat: finished cardgame slider

--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -1,8 +1,60 @@
+import { ArrowBackIos, ArrowForwardIos } from '@styled-icons/material-outlined'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import Slider, { SliderSettings } from 'components/Slider'
+
 import * as S from './styles'
 
-const GameCardSlider = () => (
-  <S.Container>
-    <h1>GameCardSlider</h1>
+export type GameCardSliderProps = {
+  items: GameCardProps[]
+  color?: 'white' | 'black'
+}
+
+const settings: SliderSettings = {
+  arrows: true,
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.2,
+      },
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.1,
+      },
+    },
+  ],
+  nextArrow: <ArrowForwardIos aria-label="next slide" />,
+  prevArrow: <ArrowBackIos aria-label="previous slide" />,
+}
+
+const GameCardSlider = ({ items, color = 'white' }: GameCardSliderProps) => (
+  <S.Container color={color}>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <GameCard {...item} key={index} />
+      ))}
+    </Slider>
   </S.Container>
 )
 
--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -1,15 +1,70 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import 'match-media-mock'
 
 import GameCardSlider from './'
 
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x145',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+]
+
 describe('<GameCardSlider />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCardSlider />)
+  it('should render 4 active items', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
 
-    expect(
-      screen.getByRole('heading', { name: /GameCardSlider/i }),
-    ).toBeInTheDocument()
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render black arrows when passed', () => {
+    renderWithTheme(<GameCardSlider color="black" items={items} />)
+
+    expect(screen.getByLabelText(/next slide/i)).toHaveStyle({
+      color: '#030517',
+    })
+
+    expect(screen.getByLabelText(/previous slide/i)).toHaveStyle({
+      color: '#030517',
+    })
   })
 })
