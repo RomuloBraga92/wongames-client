ff43ff2387f54f9553cbdd01fe52f5f82cbf9d94
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-13 17:17:18 -0300

    feat: add static items to home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,8 +1,43 @@
+import { Container } from 'components/Container'
+import Footer from 'components/Footer'
+import Heading from 'components/Heading'
+import Menu from 'components/Menu'
+
 import * as S from './styles'
 
 const Home = () => (
   <S.Container>
-    <h1>Home</h1>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <Heading leftLine lineColor="secondary" color="black">
+        News{' '}
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading leftLine lineColor="secondary" color="white">
+        Most Populars
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading leftLine lineColor="secondary" color="white">
+        Upcoming
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading leftLine lineColor="secondary" color="white">
+        Free games
+      </Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
   </S.Container>
 )
 
