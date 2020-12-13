8d9cc498cfcd9321cc0b11f3d1c3385277ce8f15
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-12 23:32:23 -0300

    feat: add banner slider styles

--- src/components/BannerSlider/styles.ts
+++ src/components/BannerSlider/styles.ts
@@ -1,3 +1,56 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+import * as BannerStyles from 'components/Banner/styles'
 
-export const Container = styled.main``
+export const Container = styled.section`
+  ${({ theme }) => css`
+    .slick-dots {
+      list-style: none;
+      display: flex !important;
+      align-items: center;
+      justify-content: center;
+      margin-top: ${theme.spacings.small};
+
+      li {
+        background: ${theme.colors.white};
+        width: 1.2rem;
+        height: 1.2rem;
+        border-radius: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 0 ${theme.spacings.xxsmall};
+        cursor: pointer;
+
+        &.slick-active {
+          background: ${theme.colors.primary};
+        }
+      }
+
+      button {
+        opacity: 0;
+        width: 1.2rem;
+        height: 1.2rem;
+        cursor: pointer;
+      }
+    }
+
+    ${media.greaterThan('large')`
+      ${BannerStyles.Container} {
+        max-width: 104rem;
+        margin: 0 auto;
+      }
+      .slick-dots {
+        position: absolute;
+        right: 0;
+        flex-direction: column;
+        height: 100%;
+        top: 0;
+        margin: 0;
+        li {
+          margin: ${theme.spacings.xxsmall} 0;
+        }
+      }
+    `}
+  `}
+`
