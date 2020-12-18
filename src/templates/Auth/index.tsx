66d85cbf55978f8cf9f54d34e95357396c9e020a
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-17 21:36:23 -0300

    feat: add Auth template and signin/up pages

---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Auth = () => (
+  <S.Container>
+    <h1>Auth</h1>
+  </S.Container>
+)
+
+export default Auth
---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+
+import Auth from './'
+
+describe('<Auth />', () => {
+  it('should render the heading', () => {
+    render(<Auth />)
+  })
+})
