006de389690a5531ecb433f4f692d425780cf403
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-25 01:56:39 -0300

    feat: add z-index to menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -11,6 +11,7 @@ export const Container = styled.menu`
     align-items: center;
     padding: ${theme.spacings.small} 0;
     position: relative;
+    z-index: ${theme.layers.menu};
   `}
 `
 export const LogoWrapper = styled.div`
