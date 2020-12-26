8c3bfd473f81be9cc3010421cf1d7abdbd8e2525
Romulo Braga <romulo.braga.92@gmail.com>
2020-12-26 00:09:56 -0300

    feat: add link to home to menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -26,12 +26,18 @@ const Menu = ({ username }: MenuProps) => {
       </MediaMatch>
 
       <S.LogoWrapper>
-        <Logo hideOnMobile />
+        <Link href="/" passHref>
+          <a>
+            <Logo hideOnMobile />
+          </a>
+        </Link>
       </S.LogoWrapper>
 
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Store</S.MenuLink>
         </S.MenuNav>
       </MediaMatch>
@@ -57,7 +63,9 @@ const Menu = ({ username }: MenuProps) => {
         <CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
 
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Store</S.MenuLink>
 
           {!!username && (
