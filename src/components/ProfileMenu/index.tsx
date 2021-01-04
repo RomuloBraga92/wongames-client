65ef410c90dbd8bb5d927c227908050fcf07a218
Romulo Braga <romulo.braga.92@gmail.com>
2021-01-04 00:56:04 -0300

    fix: case sensitive

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link'
 import * as S from './styles'
 
 export type ProfileMenuProps = {
-  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders'
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders' | string
 }
 
 const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
