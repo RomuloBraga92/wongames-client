ffe3dc9bd56c27283543f5cb5ec4986daf736781
Romulo Braga <romulo.braga.92@gmail.com>
2021-01-24 20:58:24 -0300

    feat: add apollo to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,30 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
+import { ApolloProvider } from '@apollo/client'
 
 import GlobalStyles from '../styles/global'
 import theme from '../styles/theme'
+import { useApollo } from 'utils/apollo'
 
 function App({ Component, pageProps }: AppProps) {
+  const client = useApollo(pageProps.initialApoloState)
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="An amazing Game Store, made for you!"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="An amazing Game Store, made for you!"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
